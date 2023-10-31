c2f90f2cf2a85b3c81a2ce4278b21ac71c8ba0dc
cin_vpereira <cin_vpereira@uolinc.com>
2023-10-30 23:18:18 -0300

    adicionando endpoint de comprar

--- gamestore/src/main/java/br/com/gamestore/gamestore/controllers/UserController.java
+++ gamestore/src/main/java/br/com/gamestore/gamestore/controllers/UserController.java
@@ -1,15 +1,23 @@
 package br.com.gamestore.gamestore.controllers;
 
+import br.com.gamestore.gamestore.entities.Purchase;
 import br.com.gamestore.gamestore.entities.User;
 import br.com.gamestore.gamestore.resourceItems.LoginFormResorceItem;
+import br.com.gamestore.gamestore.resourceItems.dto.PurchaseDto;
 import br.com.gamestore.gamestore.resourceItems.dto.UserDto;
+import br.com.gamestore.gamestore.services.PurchaseService;
 import br.com.gamestore.gamestore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,9 +26,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PurchaseService purchaseService;
+
     @GetMapping(value = "/login")
     public ResponseEntity<?> login(@RequestBody LoginFormResorceItem loginForm) throws Throwable {
-        UserDto userItem =  userService.findByUser(loginForm);
-        return ResponseEntity.ok(userItem);
+        Optional<User> userItem = userService.findByUser(loginForm);
+        userItem.get().setPurchases(purchaseService.findAllByUserId(userItem.get().getId()));
+        UserDto userDto = new UserDto(userItem);
+        return ResponseEntity.ok(userDto);
+    }
+
+    @GetMapping(value = "/purchases")
+    public ResponseEntity<?> getPurchasesByUser(@RequestParam Integer userId) {
+        List<Purchase> purchaseList = purchaseService.findAllByUserId(userId);
+        if (purchaseList.isEmpty()) {
+            return ResponseEntity.notFound().header("message",
+                    "Nenhuma compra foi encontrada para usuario id:".concat(String.valueOf(userId))).build();
+        }
+        List<PurchaseDto> purchaseDtos = purchaseList.stream()
+                .map(PurchaseDto::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(purchaseDtos);
     }
 }
--- gamestore/src/main/java/br/com/gamestore/gamestore/services/UserService.java
+++ gamestore/src/main/java/br/com/gamestore/gamestore/services/UserService.java
@@ -1,17 +1,12 @@
 package br.com.gamestore.gamestore.services;
 
-import br.com.gamestore.gamestore.entities.Purchase;
 import br.com.gamestore.gamestore.entities.User;
-import br.com.gamestore.gamestore.repositories.PurchaseRepository;
 import br.com.gamestore.gamestore.repositories.UserRepository;
 import br.com.gamestore.gamestore.resourceItems.LoginFormResorceItem;
-import br.com.gamestore.gamestore.resourceItems.dto.UserDto;
 import br.com.gamestore.gamestore.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,13 +15,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PurchaseRepository purchaseRepository;
-
-    public UserDto findByUser(LoginFormResorceItem userResourceItem) throws Throwable {
+    public Optional<User> findByUser(LoginFormResorceItem userResourceItem) throws Throwable {
         Optional<User> userEntity = userRepository.findByUsernameAndPassword(userResourceItem.getUsername(), userResourceItem.getPassword());
-        userEntity.orElseThrow(() -> new ObjectNotFoundException("Login não encontrado"));
-        userEntity.get().setPurchases(purchaseRepository.findAllByUserId(userEntity.get().getId()));
-        return new UserDto(userEntity);
+        return Optional.ofNullable(userEntity.orElseThrow(() -> new ObjectNotFoundException("Login não encontrado")));
     }
 }
