154da27a19a00903a15e0e9d0e61ce9f7f0a67fe
cin_vpereira <cin_vpereira@uolinc.com>
2023-11-15 21:06:56 -0300

    Adicionando endpoints

--- gamestore/src/app.py
+++ gamestore/src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
-from bson import json_util
+from bson import json_util, ObjectId 
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/gamestoredb'
@@ -8,49 +8,71 @@ mongo = PyMongo(app)
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    # Obter parâmetros de username e senha do corpo da solicitação
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-
-    # Verificar se o usuário existe na base de dados
     user = mongo.db.usuarios.find_one({'username': username, 'senha': password})
-
     if user:
-        # Usuário autenticado com sucesso
         return jsonify({'message': 'Login bem-sucedido'}), 200
     else:
-        # Usuário não encontrado ou senha incorreta
         return jsonify({'message': 'Credenciais inválidas'}), 401
-
 
 @app.route('/api/usuarios', methods=['GET'])
 def get_usuarios():
-    # Exemplo de consulta ao MongoDB
     usuarios_cursor = mongo.db.usuarios.find()
-
-    # Converte o cursor para uma lista
     usuarios = list(usuarios_cursor)
-
-    # Serializa a lista para JSON usando json_util
     json_usuarios = json_util.dumps(usuarios)
-
-    # Retorna a resposta JSON
     return json_usuarios, 200, {'Content-Type': 'application/json'}
 
 @app.route('/api/produtos', methods=['GET'])
 def get_produtos():
-    # Exemplo de consulta ao MongoDB
     produtos_cursor = mongo.db.produtos.find()
-
-    # Converte o cursor para uma lista
     produtos = list(produtos_cursor)
-
-    # Serializa a lista para JSON usando json_util
     json_produtos = json_util.dumps(produtos)
-
-    # Retorna a resposta JSON
     return json_produtos, 200, {'Content-Type': 'application/json'}
+
+@app.route('/api/produtos', methods=['POST'])
+def cadastrar_produto():
+    data = request.get_json()
+    if 'product_name' not in data or 'product_desc' not in data or 'product_price' not in data:
+        return jsonify({'message': 'Campos obrigatórios ausentes'}), 400
+    produto = {
+        'product_name': data['product_name'],
+        'product_desc': data['product_desc'],
+        'product_price': data['product_price']
+    }
+    produto_inserido = mongo.db.produtos.insert_one(produto)
+    return jsonify({'message': 'Produto cadastrado com sucesso', 'product_id': str(produto_inserido.inserted_id)}), 201
+
+@app.route('/api/produtos/<string:product_id>', methods=['PUT'])
+def alterar_produto(product_id):
+    data = request.get_json()
+    if 'product_name' not in data or 'product_desc' not in data or 'product_price' not in data:
+        return jsonify({'message': 'Campos obrigatórios ausentes'}), 400
+    produto_atualizado = {
+        'product_name': data['product_name'],
+        'product_desc': data['product_desc'],
+        'product_price': data['product_price']
+    }
+    mongo.db.produtos.update_one({'_id': ObjectId(product_id)}, {'$set': produto_atualizado})
+    return jsonify({'message': 'Produto alterado com sucesso'}), 200
+
+@app.route('/api/produtos/<string:product_id>', methods=['GET'])
+def obter_produto_por_id(product_id):
+    produto = mongo.db.produtos.find_one({'_id': ObjectId(product_id)})
+    if produto:
+        produto['_id'] = str(produto['_id'])
+        return jsonify(produto), 200
+    else:
+        return jsonify({'message': 'Produto não encontrado'}), 404
+
+@app.route('/api/produtos/<string:product_id>', methods=['DELETE'])
+def deletar_produto(product_id):
+    produto = mongo.db.produtos.find_one({'_id': ObjectId(product_id)})
+    if not produto:
+        return jsonify({'message': 'Produto não encontrado'}), 404
+    mongo.db.produtos.delete_one({'_id': ObjectId(product_id)})
+    return jsonify({'message': 'Produto deletado com sucesso'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
